d2ee17b21adcf2c12cf6eed69b72e8a92cc186a4
famila <famila@pop-os.localdomain>
2022-05-31 18:05:02 -0300

    integraçao com backend

---
+++ src/app/components/product/product-creat/product-creat.component.ts
@@ -0,0 +1,32 @@
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service'; 
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-product-creat',
+  templateUrl: './product-creat.component.html',
+  styleUrls: ['./product-creat.component.css']
+})
+export class ProductCreatComponent implements OnInit {
+
+  product : Product = {
+    name:'',
+    price: null!
+
+  }
+  constructor(private productService: ProductService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    
+    }
+    createProduct(): void{
+      this.productService.create(this.product).subscribe(()=> {
+      this.productService.showMessage('Produto Criado')
+      this.router.navigate(['/products'])
+      })
+    }
+    Cancel(): void{
+      this.router.navigate(['/products'])
+}
+}
---
+++ src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,25 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-read',
+  templateUrl:'./product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+ 
+  products: Product[] = [];
+  displayedColumns= ['id', 'name' , 'price'];
+  constructor(private productSevice: ProductService) {}
+
+  ngOnInit(): void {
+    this.productSevice.read().subscribe(products =>{
+      
+      this.products=products
+      
+    })
+
+  }
+
+}
